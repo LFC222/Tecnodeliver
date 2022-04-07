8f953fad99937f3f51223704afa664bc5b2bec02
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-04-07 02:14:11 -0300

    Projeto criado

---
+++ backend/src/main/java/com/example/tecnodeliver/TecnodeliverApplication.java
@@ -0,0 +1,13 @@
+package com.example.tecnodeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnodeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnodeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/tecnodeliver/TecnodeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tecnodeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnodeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
