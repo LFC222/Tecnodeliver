f3b93cccfcf625c00b678e3497e60dcef4c82497
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-04-07 17:09:15 -0300

    Put/orders/{id}/delivered

--- backend/src/main/java/com/example/tecnodeliver/services/OrderService.java
+++ backend/src/main/java/com/example/tecnodeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.tecnodeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.tecnodeliver.dto.OrderDTO;
+import com.example.tecnodeliver.dto.ProductDTO;
 import com.example.tecnodeliver.entities.Order;
+import com.example.tecnodeliver.entities.OrderStatus;
+import com.example.tecnodeliver.entities.Product;
 import com.example.tecnodeliver.repositories.OrderRepository;
+import com.example.tecnodeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,32 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),dto.getLongitude(),Instant.now(),OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getById(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id){
+		Order order = repository.getById(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
