c4d3bd6950f4bb871f13e17322b860d9f0b8fb92
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-04-07 05:58:13 -0300

    Get/Orders

---
+++ backend/src/main/java/com/example/tecnodeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.example.tecnodeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.tecnodeliver.dto.OrderDTO;
+import com.example.tecnodeliver.entities.Order;
+import com.example.tecnodeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
