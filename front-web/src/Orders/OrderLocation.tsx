5a71dc6f1e73de693f88c4af85e8ddfdf64e536b
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-04-21 19:39:26 -0300

    Finish

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import AsyncSelect from 'react-select/async';
-import { fetchLocalMapBox } from "../api";
 import { OrderLocationData } from "./types";
 
 
@@ -30,22 +29,7 @@ function OrderLocation({onChangeLocation} : Props) {
   });
 
 
-  const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
-    const response = await fetchLocalMapBox(inputValue);
   
-    const places = response.data.features.map((item: any) => {
-      return ({
-        label: item.place_name,
-        value: item.place_name,
-        position: {
-          lat: item.center[1],
-          lng: item.center[0]
-        }
-      });
-    });
-  
-    callback(places);
-  };
   const handleChangeSelect = (place: Place) => {
     setAddress(place);
     onChangeLocation({
